9a8ce3d8ba8bdcbcd34b9c9208e33d8b489c983a
Hugh A. Miles II <hmiles23@ufl.edu>
2016-02-02 20:10:29 -0500

    removed error in viewController.swift

--- Twitter/ViewController.swift
+++ Twitter/ViewController.swift
@@ -22,9 +22,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onLogin(sender: AnyObject) {
-        TwitterClient.sharedInstance.loginWithBlock() {
-            
-        }
+
         TwitterClient.sharedInstance.requestSerializer.removeAccessToken()
         TwitterClient.sharedInstance.fetchRequestTokenWithPath("oauth/request_token", method: "GET", callbackURL: NSURL(string: "cptwitterdemo://oauth"), scope: nil, success: { (requestToken: BDBOAuth1Credential!) -> Void in
             print("Got the request token")
