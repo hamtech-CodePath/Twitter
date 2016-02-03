7888abc1c0ec2a3bd8430a93662f9fdae9523b70
Hugh A. Miles II <hmiles23@ufl.edu>
2016-02-02 20:08:50 -0500

    updated ignore

---
+++ TwitterClient.swift
@@ -0,0 +1,13 @@
+//
+//  TwitterClient.swift
+//  Twitter
+//
+//  Created by Hugh A. Miles II on 2/2/16.
+//  Copyright © 2016 Hugh A. Miles II. All rights reserved.
+//
+
+import UIKit
+
+class TwitterClient: BDBOAuth1Credential {
+
+}
